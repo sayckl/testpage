48ac5b48e399feb577206ed734e325ea8a7e92f6
sayckl <dr.gauss196@gmail.com>
2019-08-02 12:36:40 +0300

    added-styling-for-smallscreen

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const slides = document.querySelectorAll('#slides .slide');
+const email = document.getElementById('email');
+const menu = document.querySelector('.menu-btn');
+const aside = document.querySelector('.main-menu');
 let currentSlide = 0;
 let slideInterval = setInterval(nextSlide,3000);
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('menu-btn_active');
+  aside.classList.toggle('show_aside');
+});
 
 function nextSlide() {
     slides[currentSlide].className = 'slide';
@@ -11,16 +19,18 @@ function nextSlide() {
 const form = document.querySelector('.support-body');
 form.addEventListener('submit', event => {
   event.preventDefault()
-  const isValid = validate(event)
-  console.log(isValid);
+  validate(event)
+  form.reset();
 })
 
 const validate = () => {
   let isValid = true
   if(!email.value.match(/^\w{1,}@\w{1,}\.\w{2,}$/)) {
     email.classList.add("error");
+    alert("Wrong mail!");
     isValid = false;
   }else{
     email.classList.remove("error");
 }
+console.log(isValid);
 }
